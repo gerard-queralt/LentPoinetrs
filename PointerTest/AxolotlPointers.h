a48d4beb0de542b14218838864d8d34a04a851c5
Gerard <gqueraltferre@gmail.com>
2023-02-01 13:24:10 +0100

    uploaded all files

---
+++ PointerTest/AxolotlPointers.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+
+namespace AxoPtr
+{
+	template<typename T>
+	class lent_ptr
+	{
+	public:
+		lent_ptr(T* rawPointer) : rawPointer(rawPointer)
+		{
+		}
+
+		lent_ptr(const std::unique_ptr<T>& uniquePointer) : lent_ptr(uniquePointer.get())
+		{
+		}
+
+		inline T* operator ->()
+		{
+			return rawPointer;
+		}
+
+		inline T& operator *()
+		{
+			return *rawPointer;
+		}
+
+		inline operator bool() const
+		{
+			return rawPointer != nullptr;
+		}
+
+		inline bool operator !() const
+		{
+			return rawPointer == nullptr;
+		}
+
+		inline T* operator =(nullptr_t) noexcept
+		{
+			return rawPointer = nullptr;
+		}
+
+		inline T* operator =(const std::unique_ptr<T>& uniquePtr) noexcept
+		{
+			return rawPointer = uniquePtr.get();
+		}
+
+		inline friend bool operator ==(const lent_ptr<T>& lent1, const lent_ptr<T>& lent2)
+		{
+			return lent1.rawPointer == lent2.rawPointer;
+		}
+
+		inline friend bool operator ==(const lent_ptr<T>& lent1, T* rawPointer)
+		{
+			return lent1.rawPointer == rawPointer;
+		}
+
+		inline friend bool operator ==(const lent_ptr<T>& lent, const std::unique_ptr<T>& unique)
+		{
+			return lent.rawPointer == unique.get();
+		}
+
+	private:
+		T* rawPointer;
+	};
+
+	template<typename T>
+	static std::vector<lent_ptr<T> > uniqueVectorToLent(const std::vector<std::unique_ptr<T> >& originalVector)
+	{
+		return std::vector<lent_ptr<T> >(std::begin(originalVector), std::end(originalVector));
+	}
+}
---
+++ PointerTest/PointerTest.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <memory>
+#include "AxolotlPointers.h"
+
+using namespace std;
+
+class A
+{
+public:
+    A(int val) : val(val)
+    {
+    }
+    ~A()
+    {
+        cout << "Deleting A with val = " << val << endl;
+    }
+    void foo()
+    {
+        cout << "Hello from foo! Val = " << val << endl;
+    }
+private:
+    int val;
+};
+
+void bar(const AxoPtr::lent_ptr<A>& lentA)
+{
+    cout << "Hello from bar!" << endl;
+}
+
+int main()
+{
+    //create unique ptr
+    std::unique_ptr<A> a = std::make_unique<A>(10);
+    //create lent ptr
+    AxoPtr::lent_ptr<A> lentA = AxoPtr::lent_ptr<A>(a.get());
+    //operator ->
+    lentA->foo();
+    //operator *
+    A test = *lentA;
+    test.foo();
+    //operator bool
+    if (lentA)
+    {
+        cout << "lent ptr not empty" << endl;
+    }
+    //operator =nullptr
+    lentA = nullptr;
+    //operator !
+    if (!lentA)
+    {
+        cout << "lent ptr empty" << endl;
+    }
+    //unique ptr to lent conversion
+    lentA = a;
+    AxoPtr::lent_ptr<A> lentA2 = a;
+    //unique ptr to lent cast
+    bar(a);
+    //vectors
+    std::vector<unique_ptr<A>> uniqueVector;
+    for (int i = 0; i < 4; ++i)
+    {
+        uniqueVector.push_back(std::make_unique<A>(i));
+    }
+    std::vector<AxoPtr::lent_ptr<A>> lentVector = AxoPtr::uniqueVectorToLent(uniqueVector);
+    for (int i = 0; i < lentVector.size(); ++i)
+    {
+        lentVector[i]->foo();
+    }
+    //find
+    std::vector<std::unique_ptr<A>>::iterator res = std::find(std::begin(uniqueVector), std::end(uniqueVector), lentVector[0]);
+    uniqueVector.erase(res);
+    cout << "Now vector has size: " << uniqueVector.size() << endl;
+    //comparison
+    cout << "lent equal to self? " << std::boolalpha << (lentA == lentA) << endl;
+    cout << "lent equal to raw? " << std::boolalpha << (lentA == a.get()) << endl;
+    cout << "lent equal to unique? " << std::boolalpha << (lentA == a) << endl;
+}
